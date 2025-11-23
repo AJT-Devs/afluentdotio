600619142c695fecfcc056933fed4c09c0fe0f1a
João Alves Passos <112397879+joaoapassos@users.noreply.github.com>
2025-11-22 21:13:12 -0300

    fix: corrigir nome da função no componente Initializer

--- src/renderer/src/others/Initializer.tsx
+++ src/renderer/src/others/Initializer.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-const  = ({ children }: { children: React.ReactNode }) => {
+const Initializer = ({ children }: { children: React.ReactNode }) => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
