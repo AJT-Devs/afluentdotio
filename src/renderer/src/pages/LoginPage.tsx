4bcfddba637e64be42bf76c4b5cb0dd6c3db7a45
tulio <tuliokenjiyoshimura@gmail.com>
2025-11-26 20:52:23 -0300

    feat: userModal

--- src/renderer/src/pages/LoginPage.tsx
+++ src/renderer/src/pages/LoginPage.tsx
@@ -8,23 +8,34 @@ import "@renderer/assets/stylesheets/pages/login-page.css";
 import { useEffect, useState, useRef } from "react";
 import { User } from "src/entities/User";
 import { SuccessResponse } from "src/entities/SuccessResponse";
+import { Plus } from "lucide-react";
+import { Dialog } from "@radix-ui/react-dialog";
 
 
 
 
-function handleLogin(userTarget: React.RefObject<HTMLDivElement | null>) {
-  const navigate = useNavigate();
-  if (userTarget.current !== null) {
-    navigate("/introduction");
-  }
-}
+
 
 const LoginPage = () => {
   
   const [users, setUsers] = useState<User[]>([]);
-  const hasFetchedUsers = useRef(false);
+  const hasFetchedUsers= useRef<boolean>(false);
+  const navigate = useNavigate();
 
-  const userTarget = useRef<HTMLDivElement>(null);
+  const [userId, setUserId] = useState<string | null>(null); 
+  const createUserRef = useRef<boolean>(false);
+
+  function handleLogin(userTarget: string | null) {
+    if (userTarget !== null) {
+      navigate("/introduction");
+    }
+  }
+
+  function openUserModal() {
+    createUserRef.current = true;
+  }
+
+
 
   useEffect(() => {
     if (hasFetchedUsers.current) return;
@@ -52,15 +63,19 @@ const LoginPage = () => {
       <AfluentLogo />
       <div className="user-list">
         {users.map((user) => (
-          <div key={user.id} className="user-card" tabIndex={0}>
+          <button key={user.id} className="user-card" tabIndex={0} onClick={() => {setUserId(user.id); console.log("Selected user ID:", user.id)} }>
             <img src={user.photo} alt={`${user.name} foto`} className="photo" />
             <h2>{user.name}</h2>
-          </div>
+          </button>
             )
         )}
+        <button className="user-card" tabIndex={0} onClick={() => openUserModal()}>
+          <Plus size={30} />
+        </button>
       </div>
+      {createUserRef && <Dialog />}
       <button
-        onClick={() => handleLogin(userTarget)} >
+        onClick={() => handleLogin(userId)} >
         LOGIN
       </button>
     </div>
