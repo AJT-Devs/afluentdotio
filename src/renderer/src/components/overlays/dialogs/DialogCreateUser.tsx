b56672cf68cd1a18fc6be01b38374f1de5a5a7b1
joaoapassos <joaoapassjp@gmail.com>
2025-11-26 21:15:23 -0300

    feat: dialogo create user

---
+++ src/renderer/src/components/overlays/dialogs/DialogCreateUser.tsx
@@ -0,0 +1,59 @@
+import DialogContent from "./Dialog";
+import * as React from "react";
+import { useState } from "react";
+
+interface DialogCreateUser {
+    open: boolean;
+    onOpenChange: (open: boolean) => void;
+    actionSubmit: (name: string, photo?: string | null) => void;
+}
+
+const DialogCreateUser = ({ 
+    open, 
+    onOpenChange,
+    actionSubmit 
+}: DialogCreateUser) => {
+    const [name, setName] = useState("");
+    const [photo, setPhoto] = useState("");
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        actionSubmit(name, (photo.trim() == "" ? null : photo));
+        onOpenChange(false);
+    };
+
+    return (
+        <DialogContent.Root open={open} onOpenChange={onOpenChange}>
+            <DialogContent.Content>
+                <DialogContent.Title>Cadastrar usuário</DialogContent.Title>
+                
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="username-input">Nome:</label>
+                    <input type="text" 
+                        name="username-input"
+                        id="username-input"
+                        placeholder="Username"
+                        onChange={(e) => setName(e.target.value)}
+                        autoFocus
+                    />
+                    <label htmlFor="photo-input">Foto de usuário:</label>
+                    <input type="text" 
+                        name="photo-input"
+                        id="photo-input"
+                        placeholder="Cole a url da sua imagem"
+                        onChange={(e) => setPhoto(e.target.value)}
+                    />
+                    <div className="dialog-buttons">
+                        <DialogContent.Close asChild>
+                            <button type="button">Cancelar</button>
+                        </DialogContent.Close>
+                        
+                        <button type="submit">Cadastrar</button>
+                    </div>
+                </form>
+            </DialogContent.Content>
+        </DialogContent.Root>
+    );
+};
+
+export default DialogCreateUser;
